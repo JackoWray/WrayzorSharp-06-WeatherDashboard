6bc1e66bab577ece17639428adff49b660a2ba35
Jackson Wray <jackson.wray19@outlook.com>
2022-01-26 20:24:48 +0800

    Added weather cond. icon, added cardinal wind direction rather than bearing

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,33 @@ var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
 var currentUV = document.getElementById("current-uv");
+var conditonIcon = $("#condition-icon");
+var directionVarArray = [
+  "N",
+  "NNE",
+  "NE",
+  "ENE",
+  "E",
+  "ESE",
+  "SE",
+  "SSE",
+  "S",
+  "SSW",
+  "SW",
+  "WSW",
+  "W",
+  "WNW",
+  "NW",
+  "NNW",
+  "N",
+];
+
 function kelvinToCelsius(kelvin) {
   return kelvin - 273.15;
 }
 function mpsToKph(mps) {
   return mps * 3.6;
 }
-
 function getCurrentWeather(city) {
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -42,18 +62,31 @@ searchForm.addEventListener("submit", function (event) {
   var city = citySearch.value;
   getCurrentWeather(city).then(function (data) {
     console.log(data);
-
+    var iconUrl =
+      "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    function bearingToCardinal(bearing) {
+      var direcitonVar = (bearing / 22.5).toFixed();
+      return directionVarArray[direcitonVar];
+    }
     currentCity.textContent =
-      "Current weather in " + data.name + ", " + data.sys.country;
+      "Current weather in " +
+      data.name +
+      ", " +
+      data.sys.country +
+      " | " +
+      data.weather[0].main +
+      " | ";
     currentTemp.textContent =
       "Temperature: " + kelvinToCelsius(data.main.temp).toFixed(2) + "°C";
     currentWind.textContent =
       "Wind (Bearing): " +
       mpsToKph(data.wind.speed).toFixed(1) +
-      "km/h (" +
-      data.wind.deg +
-      "°)";
+      "km/h " +
+      bearingToCardinal(data.wind.deg);
     currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+
+    conditonIcon.attr("src", iconUrl);
+    conditonIcon.attr("alt", "Weather Icon");
 
     getUVI(data.coord.lat, data.coord.lon).then(function (data) {
       console.log(data);
